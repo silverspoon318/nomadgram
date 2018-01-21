9b11148e093e35f59b81c302992311d2547e43d4
sungwonkim <sungwon3708@gmail.com>
2018-01-21 20:27:51 +0900

    customizing django(display, filter, search...)

--- nomadgram/images/admin.py
+++ nomadgram/images/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Image)
+class ImageAdmin(admin.ModelAdmin):
+    # pass    # 텅 빈 클래스를 의미한다.
+
+    list_display_links = (
+        'location',
+    )
+
+    search_fields = (
+        'location',
+        'caption',
+    )
+
+    list_filter = (
+        'location',
+        'creator',
+    )
+
+    list_display = (
+        'file',
+        'location',
+        'caption',
+        'creator',
+        'created_dt',
+        'updated_dt',
+    )
+
+@admin.register(models.Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display =  (
+        'creator',
+        'image',
+        'created_dt',
+        'updated_dt',
+    )
+
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        'message',
+        'creator',
+        'image',
+        'created_dt',
+        'updated_dt',
+    )
--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,14 +1,17 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 from nomadgram.users import models as user_models
 
+@python_2_unicode_compatible
 class TimeStampedModel(models.Model):
     
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_dt = models.DateTimeField(auto_now_add=True)
+    updated_dt = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True #이 모델은 데이터베이스를 생성하기 위해 사용되지 않는다., 다른 모델들을 위한 base로 사용된다.
 
+@python_2_unicode_compatible
 class Image(TimeStampedModel):
 
     """ Image Model """
@@ -16,8 +19,13 @@ class Image(TimeStampedModel):
     file = models.ImageField()
     location = models.CharField(max_length = 140)
     caption = models.TextField()
-    image = models.ForeignKey(user_models.User, null=True) # image 를 생성한 사람
+    creator = models.ForeignKey(user_models.User, null=True) # image 를 생성한 사람
 
+    # meta class, string representation : string 이 어떻게 보이느냐
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
+
+@python_2_unicode_compatible
 class Comment(TimeStampedModel):
     
     """ Comment Model """
@@ -26,9 +34,16 @@ class Comment(TimeStampedModel):
     creator = models.ForeignKey(user_models.User, null=True)
     image = models.ForeignKey(Image, null=True)
 
+    def __str__(self):
+        return self.message
+
+@python_2_unicode_compatible
 class Like(TimeStampedModel):
 
     """ Like Model """
 
     creator = models.ForeignKey(user_models.User, null=True)
-    image = models.TextField(Image, null=True)
+    image = models.ForeignKey(Image, null=True)
+
+    def __str__(self):
+        return 'User: {} - Image Caption: {}'.format(self.creator.username, self.image.caption)
