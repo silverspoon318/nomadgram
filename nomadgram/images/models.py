226b419f7ca70a07ede4702abca76b80fed43062
sungwonkim <sungwon3708@gmail.com>
2018-01-21 18:17:29 +0900

    create like, comment creator image

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from nomadgram.users import models as user_models
 
 class TimeStampedModel(models.Model):
     
@@ -10,10 +11,24 @@ class TimeStampedModel(models.Model):
 
 class Image(TimeStampedModel):
 
+    """ Image Model """
+
     file = models.ImageField()
     location = models.CharField(max_length = 140)
     caption = models.TextField()
+    image = models.ForeignKey(user_models.User, null=True) # image 를 생성한 사람
 
 class Comment(TimeStampedModel):
     
+    """ Comment Model """
+
     message = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True)
+    image = models.ForeignKey(Image, null=True)
+
+class Like(TimeStampedModel):
+
+    """ Like Model """
+
+    creator = models.ForeignKey(user_models.User, null=True)
+    image = models.TextField(Image, null=True)
