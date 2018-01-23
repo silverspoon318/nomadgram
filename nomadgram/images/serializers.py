cb4fcad17a5ae1f4777a2fd0ae9eb4cb3cbf70f4
sungwonkim <sungwon3708@gmail.com>
2018-01-24 05:41:17 +0900

    update : imageserializer nested, add

--- nomadgram/images/serializers.py
+++ nomadgram/images/serializers.py
@@ -9,11 +9,16 @@ class ImageSerializer(serializers.ModelSerializer):
 
 class CommentSerializer(serializers.ModelSerializer):
     
+    image = ImageSerializer()
+
     class Meta:
         model = models.Comment
         fields = '__all__'
 
 class LikeSerializer(serializers.ModelSerializer):
+
+    # nested serialize, foreign key 가 아니라 이미지 시리얼라이저, 이미 시리얼라이즈가 구축되어 있기 때문에 키가 아닌 시리얼라이즈로 노출된다.
+    image = ImageSerializer()
 
     class Meta:
         model = models.Like
